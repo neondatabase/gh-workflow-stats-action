e5fd50445852a7136dccf3c0ce9887af7db96336
Fedor Dikarev <fedor.dikarev@gmail.com>
2024-10-14 11:40:55 +0200

    gofmt history-exporter

--- cmd/history-exporter/main.go
+++ cmd/history-exporter/main.go
@@ -26,7 +26,7 @@ func main() {
 
 	if startDateStr == "" {
 		startDate = time.Now().Truncate(24 * time.Hour)
-	}else {
+	} else {
 		var err error
 		startDate, err = time.Parse("2006-01-02", startDateStr)
 		if err != nil {
@@ -36,7 +36,7 @@ func main() {
 
 	if endDateStr == "" {
 		endDate = startDate.AddDate(0, 0, 1)
-	}else {
+	} else {
 		var err error
 		endDate, err = time.Parse("2006-01-02", endDateStr)
 		if err != nil {
@@ -58,14 +58,14 @@ func main() {
 	ctx := context.Background()
 
 	durations := []time.Duration{
-		6 * time.Hour,	// 18:00 - 24:00
-		3 * time.Hour,  // 15:00 - 18:00
-		1 * time.Hour,  // 14:00 - 15:00
-		1 * time.Hour,  // 13:00 - 14:00
-		1 * time.Hour,  // 12:00 - 13:00
-		2 * time.Hour,  // 10:00 - 12:00
-		4 * time.Hour,	// 06:00 - 10:00
-		6 * time.Hour,	// 00:00 - 06:00
+		6 * time.Hour, // 18:00 - 24:00
+		3 * time.Hour, // 15:00 - 18:00
+		1 * time.Hour, // 14:00 - 15:00
+		1 * time.Hour, // 13:00 - 14:00
+		1 * time.Hour, // 12:00 - 13:00
+		2 * time.Hour, // 10:00 - 12:00
+		4 * time.Hour, // 06:00 - 10:00
+		6 * time.Hour, // 00:00 - 06:00
 	}
 	curDurIdx := 0
 	for date := endDate.Add(-durations[curDurIdx]); date.Compare(startDate) >= 0; date = date.Add(-durations[curDurIdx]) {
@@ -76,7 +76,7 @@ func main() {
 		}
 		fetchedRunsKeys := make([]gh.WorkflowRunAttemptKey, len(runs))
 		i := 0
-		for key := range(runs) {
+		for key := range runs {
 			fetchedRunsKeys[i] = key
 			i++
 		}
@@ -89,17 +89,17 @@ func main() {
 			fmt.Printf("Close to rate limit, remaining: %d", rate.Remaining)
 			fmt.Printf("Sleep till %v (%v seconds)\n", rate.Reset, time.Until(rate.Reset.Time))
 			time.Sleep(time.Until(rate.Reset.Time) + 10*time.Second)
-		}else {
+		} else {
 			fmt.Printf("Rate: %+v\n", rate)
 		}
-		for _, key := range(notInDb) {
+		for _, key := range notInDb {
 			conf.RunID = key.RunId
 			fmt.Printf("Saving runId %d Attempt %d. ", key.RunId, key.RunAttempt)
 			var attemptRun *github.WorkflowRun
 			var ok bool
 			if attemptRun, ok = runs[gh.WorkflowRunAttemptKey{RunId: key.RunId, RunAttempt: key.RunAttempt}]; ok {
 				fmt.Printf("Got it from ListWorkflowRuns results. ")
-			}else {
+			} else {
 				fmt.Printf("Fetching it from GH API. ")
 				attemptRun, _ = gh.GetWorkflowAttempt(ctx, conf, key.RunAttempt)
 			}
